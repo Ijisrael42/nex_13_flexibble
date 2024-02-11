e5cd043992122a0b7ea0f4c94d2fc736b0cdafdd
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-11 19:29:54 +0200

    new grafbase init

---
+++ app/components/Footer.tsx
@@ -0,0 +1,66 @@
+import { footerLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const FooterColumn = ({title, links}: ColumnProps) => (
+  <div className="footer_coluumn">
+    <h4 className="font-semibold">Title</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => <Link href="/" key={link}>{link}</Link>)}
+    </ul>
+  </div>
+)
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+}
+
+const Footer = () => {
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibble"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+          Flexibble is the world's leading community for creatives to share, grow, and gett hired.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap gap-12">
+
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
+          <div className="flex-1 flex flex-col gap-4">
+
+            <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+
+          <div className="flex-1 flex flex-col gap-4">
+
+            <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+            <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@ 2023  Flexible. All rights reserved</p>
+        <p className="text-gray font-semibold">
+          <span className="text-black">10,214</span> projects submitted
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
