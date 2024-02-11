029dc025a888f8be26dc34fbb223de2f0cf1e03c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-11 20:29:54 +0200

    Grafbase updated User schema

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,15 +1,10 @@
 import { graph, config } from '@grafbase/sdk'
-import { UserSchema } from './user';
-import { ProjectSchema } from './project';
 
 // Welcome to Grafbase!
 //
 // Configure authentication, data sources, resolvers and caching for your GraphQL API.
 
 const g = graph.Standalone()
-
-g.type('User', UserSchema);
-g.type('Project', ProjectSchema)
 
 // Data Sources - https://grafbase.com/docs/connectors
 //
@@ -22,6 +17,26 @@ g.type('Project', ProjectSchema)
 //   returns: g.string(),
 //   resolver: 'hello-world',
 // })
+
+const User = g.type('User', {
+  name: g.string(),//.length({ min: 2, max: 20 }),
+  email: g.string(),//.unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedUrl: g.url().optional(),
+  // projects: g.ref(Project).list().optional(),
+})
+
+const Project = g.type('Project', {
+  title: g.string(),//.length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),//.search(),
+  createdBy: g.ref(User)
+})
 
 export default config({
   graph: g,
