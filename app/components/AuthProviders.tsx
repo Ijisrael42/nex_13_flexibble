b41258e88aef34512a6c060db66c456b0535ed18
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-12 11:37:13 +0200

    Updated AuthProvider

--- app/components/AuthProviders.tsx
+++ app/components/AuthProviders.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
+"use client";
+
+import { getProviders, signIn } from 'next-auth';
+import { useState, useEffect } from 'react';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signInUrl: string;
+  callbackUrl: string;
+  signUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res)
+      setProviders(res);
+    }
+
+    fetchProviders();
+  })
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider, i) => (
+          <button key={i}>{ provider.id }</button>
+        ))}
+      </div>
+    )
+  }
   return (
     <div>AuthProviders</div>
   )
